d0bc32a8a15ab52e2dbac77b4dc03fa489445b01
Sateeshbhatt116 <sateesh.bhatt.cs28@iilm.edu>
2026-09-08 09:43:56 +0530

    Solved: Goat Latin [Easy]

---
+++ LeetCode/Easy/Goat Latin/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder ans = new StringBuilder();
+        String vowels = "aeiouAEIOU";
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (vowels.indexOf(word.charAt(0)) == -1) {
+                word = word.substring(1) + word.charAt(0);
+            }
+            word = word + "ma";
+            for (int j = 0; j <= i; j++) {
+                word = word + "a";
+            }
+            ans.append(word);
+            if (i != words.length - 1) {
+                ans.append(" ");
+            }
+        }
+        return ans.toString();
+    }
+}
